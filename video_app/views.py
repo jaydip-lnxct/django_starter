24e120bdc7dd3525b9e6ede7fbf25c7789ad965c
viren-lnxct <viren@lnxct.com>
2022-05-29 11:18:44 +0530

    Template setup done

--- video_app/views.py
+++ video_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def Home(request):
-    return render(request,'video_app/base.html')
+    return render(request,'video_app/index.html')
